052c69a721b35dc266250c389763e4982f6fd751
Cait Payne <caitpayne@outlook.com>
2018-03-28 00:20:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 function add(x, y) {
-  return x + y
+  return x + y;
 }
 
 function subtract(x, y) {
-  return x - y
+  return x - y;
 }
 
 function multiply(x, y) {
-  return x * y
+  return x * y;
 }
 
 function divide(x,y) {
-  return x / y
+  return x / y;
 }
 
 function inc(n) {
-  return n += 1
+  return n += 1;
 }
 
 function dec(n) {
-  return n -= 1
+  return n -= 1;
 }
 
 function makeInt(n) {
-parseInt('n', 10);
- return n
+return parseInt(n, 10);
 }
 
 function preserveDecimal(n) {
-  parseFloat('n');
-  return n 
+  return parseFloat(n);
+  
 }
